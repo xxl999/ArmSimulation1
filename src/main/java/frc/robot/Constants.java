194d42156884747756d2c9ca72e441ebca7758c5
xxl999 <isilcagla.2026@gmail.com>
2025-12-02 23:14:40 +0300

    Modify arm constants for PID and encoder settings
    Updated arm constants for PID control and encoder calculations.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,18 +15,15 @@ public class Constants {
   public static final String kArmPositionKey = "ArmPosition";
   public static final String kArmPKey = "ArmP";
 
-  // The P gain for the PID controller that drives this arm.
   public static final double kDefaultArmKp = 50.0;
   public static final double kDefaultArmSetpointDegrees = 75.0;
 
-  // distance per pulse = (angle per revolution) / (pulses per revolution)
-  //  = (2 * PI rads) / (4096 pulses)
-  public static final double kArmEncoderDistPerPulse = 2.0 * Math.PI / 4096;
+  public static final double kArmEncoderDistPerPulse = 2.0 * Math.PI / 4096
 
-  public static final double kArmReduction = 200;
-  public static final double kArmMass = 8.0; // Kilograms
+  public static final double kArmReduction = 10.0;
+  public static final double kArmMass = 8.0; 
   public static final double kArmLength = Units.inchesToMeters(30);
-  public static final double kMinAngleRads = Units.degreesToRadians(-75);
+  public static final double kMinAngleRads = Units.degreesToRadians(-50);
   public static final double kMaxAngleRads = Units.degreesToRadians(255);
 
 }
